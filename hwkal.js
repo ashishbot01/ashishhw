5517053a464d5ba05d8836c5fcfa0a41751e0576
ashishbot01 <ashishkkr99@gmail.com>
2023-02-13 22:46:34 +0530

    ashishhw

---
+++ hwkal.js
@@ -0,0 +1,74 @@
+// Buat Lu Yang Jual Sc Ini Yang Jujur Jangan Samp Nipu Apalagi Lari Dari Tanggung Jawab
+
+// Base Haikal Hw Mods : Copyan Dari Base Dika
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.autoavailable = false //status online (online)
+global.wlcm = []
+global.ntnsfw = []
+global.ntnsfww = []
+
+//gausah di apa² in!
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+//=================================================//
+//—————「 Set Kebutuhan Button & Kontak 」—————//
+//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
+global.youtube = 'https://chat.whatsapp.com/Jh0EznQhxbkCHIre0L2bTr' // ubah aia
+global.ig = '@hikal857' // ubah aja
+global.email = 'denyp857@gmail.com'
+global.region = 'indonesia'
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'ashish💖' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'ashish💖' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'ashish💖' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//=================================================//
+// Other
+global.owner = ['919122814903'] // ubah aja pake nomor lu
+global.premium = ['919122814903'] // ubah aja pake nomor lu
+global.packname = 'ashish💖' // ubah aja
+global.ownerr = ['ashish💖'] // ubaha aja
+global.author = 'ashish💖' //ubah aja
+global.sessionName = './sessionye/haikal' //Gausaj Di Ubah
+global.prefa = ['','!','.',',','🐤','🗿'] // Gausah Mendingan 
+global.sp = '⭔' // Gausah Juga
+
+// Terserah
+global.mess = {
+    nsfw: '*NSFW feature is not activated, group admin chat to be activated *_',
+    wait: '*Loading... Sabar Wibu*_',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'Special Features for Bot Number Users',    
+}
+//=================================================//
+//Gausah Di Apa2 in
+global.f1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.f2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.f4 = 'application/zip'
+global.f5 = 'application/pdf'
+global.f6 = 'application/vnd.android.package-archive'
+//=================================================//
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+//=================================================//
+//Terserah Kalau Paham 
+global.thumb = fs.readFileSync("./baseikal/image/hw.jpg")
+global.log0 = fs.readFileSync("./baseikal/image/haikal.jpg")
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
